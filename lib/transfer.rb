cb0d1d5944e8fa7ac8a458922563e5899a7791bb
Adam Fudge <coolfudge@gmail.com>
2017-10-20 19:52:01 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,7 @@ class Transfer
   def reverse_transfer
     if @status == 'complete'
       @sender.deposit(@amount)
-      @receiver.deposit(@amount)
+      @receiver.withdraw(@amount)
       @status == 'pending'
     end
   end
