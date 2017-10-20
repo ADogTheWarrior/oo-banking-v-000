c4eea1a6ffab967e1e6bfe83aad5e80ee0804163
Adam Fudge <coolfudge@gmail.com>
2017-10-20 19:49:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,14 +16,15 @@ class Transfer
   end
 
   def execute_transaction
-
-    if @sender.balance > @amount
-      @sender.withdraw(@amount)
-      @receiver.deposit(@amount)
-      @status = 'complete'
-    else
-      @status = 'rejected'
-      "Transaction rejected. Please check your account balance."
+    if @status == 'pending'
+      if @sender.balance > @amount
+        @sender.withdraw(@amount)
+        @receiver.deposit(@amount)
+        @status = 'complete'
+      else
+        @status = 'rejected'
+        "Transaction rejected. Please check your account balance."
+      end
     end
   end
 
